a310cc6185ee2979605d6324b853ebeee3991fee
Shem Nduati <nduatishem@gmail.com>
2024-06-09 12:19:19 +0300

    done seeding the databases

--- database/migrations/2024_06_08_160411_create_groups_table.php
+++ database/migrations/2024_06_08_160411_create_groups_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
 
         Schema::create('group_users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->foreignId('group_id')->constrained('groups')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
