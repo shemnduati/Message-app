52d605563c97fa65ba750801c2013808d7f28c48
Shem Nduati <nduatishem@gmail.com>
2024-06-11 00:30:52 +0300

    done working with the chatlayout

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,6 +1,8 @@
+import TextInput from "@/Components/TextInput";
 import { usePage } from "@inertiajs/react";
-import Echo from "laravel-echo";
 import { useEffect, useState } from "react";
+import { PencilSquareIcon  } from "@heroicons/react/24/solid";
+import ConversationItem from "@/Components/App/ConversationItem";
 
 
 const ChatLayout = ({ children }) => {
@@ -9,12 +11,23 @@ const ChatLayout = ({ children }) => {
     const selectedConversation = page.props.selectedCoversation;
     const [localConversations, setLocalConversatioins] = useState([]);
     const [sortedConversations, setSortedConversations] = useState([]);
-    const [onlineUSers, setOnlineUsers] =  useState({});
+    const [onlineUsers, setOnlineUsers] =  useState({});
 
-    const isUserOnline = (userId) => onlineUSers[userId];
+    const isUserOnline = (userId) => onlineUsers[userId];
 
     console.log("conversations", conversations);
     console.log("selectedConversations", selectedConversation);
+
+    const OnSearch = (ev) =>  {
+        const search = ev.target.value.toLowerCase();
+        setLocalConversatioins(
+            conversations.filter((conversation) => {
+                return (
+                    conversation.name.toLowerCase().includes(search)
+                );
+            })
+        )
+    }
 
     useEffect(() =>{
         setLocalConversatioins(conversations);
@@ -97,9 +110,49 @@ const ChatLayout = ({ children }) => {
     <>
         <div className="flex-1 w-full flex overflow-hidden">
             <div
-                className={`transition-all w-full sm:w-[200px] md:w-[300px]`}
+                className={`transition-all w-full sm:w-[200px] md:w-[300px] bg-slate-800
+                    flex flex-col overflow-hidden ${
+                        selectedConversation ? "-ml-[100%] sm:ml-0" : ""
+                    }`}
             >
-
+                <div className="flex items-center justify-between py-2 text-xl
+                font-medium">
+                    My Conversations
+                    <div
+                        className="tooltip tooltip-left"
+                        data-tip="Create new Group"
+                    >
+                        <button
+                            onClick={(ev) => setShowGroupModal(true)}
+                            className="text-gray-400 hover:text-gray-200"
+                        >
+                            <PencilSquareIcon className="w-4 h-4 inline-block ml-2" />
+                        </button>
+                    </div>
+                </div>
+                <div className="p-3">
+                    <TextInput
+                        onKeyUp={OnSearch}
+                        placeholder="Filter users and groups"
+                        className="w-full"
+                    />
+                </div>
+                <div className="flex-1 overflow-auto">
+                    {sortedConversations &&
+                        sortedConversations.map((conversation) => (
+                            <ConversationItem 
+                                key={`${
+                                    conversation.is_group
+                                     ? "group_"
+                                     : "user_"
+                                }${conversation.id}`}
+                                conversation={conversation}
+                                online={!!isUserOnline(conversation.id)}
+                                selectedConversation={selectedConversation}
+                             />
+                        ))
+                    }
+                </div>
             </div>
             <div className="flex-1 flex flex-col overflow-hidden">
                 {children}
