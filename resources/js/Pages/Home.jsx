f1a46af67c2490a2269cc37a4341c5bff5c669cd
Shem Nduati <nduatishem@gmail.com>
2024-06-14 20:49:54 +0300

    working with scrollable section

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,17 +1,56 @@
 import ChatLayout from '@/Layouts/ChatLayout';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
 
 
 function Home({ messages }) {
-    cosnt [localMessages, setLocalMessages] = useState([]);
+    const [localMessages, setLocalMessages] = useState([]);
+    const messagesCtrRef = useRef(null);
 
     useEffect(() => {
         setLocalMessages(messages)
     }, [messages]);
     return (
         <>
-        Messages
+        (!messages && (
+            <div className="flex flex-col gap-8 justify-center items-center  text-center
+                            h-full opacity-35">
+                <div className="text-2xl md:text-4xl p-16 text-slate-200">
+                    Please select conversation to see message
+                </div>
+                <ChatBubbleLeftRightIcon className="w-32 h-32 inline-block" />
+            </div>
+        ))
+        (messages && (
+            <>
+                <ConversationHeader 
+                    selectedConversation={selectedConversation}
+                 />
+                 <div
+                    ref={messagesCtrRef}
+                    className="flex-1 overflow-y-auto p-5"
+                    >
+                        {/* Messages */}
+                        {localMessages.length === 0 && (
+                            <div className="flex justify-center items-center h-full">
+                                No Message found
+                            </div>
+                        )}
+                        {localMessages.length > 0 && (
+                            <div className="flex-1 flex flex-col">
+                                {localMessages.map((message) =>{
+                                    <MessageItem
+                                        key={message.id}
+                                        message={message}
+                                    />
+                                })}
+                            </div>
+                        )}
+                    </div>
+                <MessageInput conversation={selectedConversation} />
+            </>
+        ))
        </>
    
     );
